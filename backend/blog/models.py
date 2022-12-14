527ffd95b8e9f506d876135570b95d179978bd73
sinanazem <sinanaazem@gmail.com>
2022-12-14 21:42:10 +0330

    add db

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,21 @@
+from distutils.command.upload import upload
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Article(models.Model):
+    STATUS_CHOICE= (
+        ('p', 'published'),
+        ('d', 'Draft')
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to = 'images')
+    pulish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICE)
+    
+    
+    def __str__(self):
+        return self.title
